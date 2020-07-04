58c1910de3d0c4363558dc0525325eafe87adfbb
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-07-04 10:34:33 +0300

    Enabled the serving of static files during development

--- django_diy_blog/urls.py
+++ django_diy_blog/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('', RedirectView.as_view(url='blog/', permanent=True)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
