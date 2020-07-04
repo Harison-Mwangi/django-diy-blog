d7a38a99063958c19857bb11cf4b47b39e8d5577
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-07-04 10:25:45 +0300

    Set the URL mapping for the homepage to be /blog/

--- django_diy_blog/urls.py
+++ django_diy_blog/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 admin.site.site_header = 'Django DIY Blog'
 admin.site.site_title = 'Django DIY Blog'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('', RedirectView.as_view(url='blog/', permanent=True)),
 ]
