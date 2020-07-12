0a2ed99c74d5a29e144a13978a8aa4cedd805f9a
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-07-12 14:23:01 +0300

    Renamed the Post model to Blog

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from blog.models import Blogger, Post, Comment
+from blog.models import Blogger, Blog, Comment
 from datetime import date
 
 class BloggerModelTest(TestCase):
@@ -38,7 +38,7 @@ class BloggerModelTest(TestCase):
     #     blogger= Blogger.objects.get(id=1)
     #     self.assertEquals(blogger.get_absolute_url(), '/blog/blogger/1')
 
-class PostModelTest(TestCase):
+class BlogModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Set up non-modified objects used by all test methods
@@ -49,47 +49,47 @@ class PostModelTest(TestCase):
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
             proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
-        Post.objects.create(title=title, description=description)
+        Blog.objects.create(title=title, description=description)
 
     # labels
     def test_title_label(self):
-        field_label = Post._meta.get_field('title').verbose_name
+        field_label = Blog._meta.get_field('title').verbose_name
         self.assertEquals(field_label, 'title')
     
     def test_author_label(self):
-        field_label = Post._meta.get_field('author').verbose_name
+        field_label = Blog._meta.get_field('author').verbose_name
         self.assertEquals(field_label, 'author')
 
     def test_date_posted_label(self):
-        field_label = Post._meta.get_field('date_posted').verbose_name
+        field_label = Blog._meta.get_field('date_posted').verbose_name
         self.assertEquals(field_label, 'date posted')
     
     def test_last_edit_label(self):
-        field_label = Post._meta.get_field('last_edit').verbose_name
+        field_label = Blog._meta.get_field('last_edit').verbose_name
         self.assertEquals(field_label, 'last edit')
 
     def test_description_label(self):
-        field_label = Post._meta.get_field('description').verbose_name
+        field_label = Blog._meta.get_field('description').verbose_name
         self.assertEquals(field_label, 'description')
 
     # max length
     def test_title_max_length(self):
-        max_length = Post._meta.get_field('title').max_length
+        max_length = Blog._meta.get_field('title').max_length
         self.assertEquals(max_length, 100)
 
     def test_description_max_length(self):
-        max_length = Post._meta.get_field('description').max_length
+        max_length = Blog._meta.get_field('description').max_length
         self.assertEquals(max_length, 4000)
 
     # custom class methods
-    def test_post_name_is_title(self):        
-        post= Post.objects.get(id=1)
-        expected_object_name = post.title
-        self.assertEquals(expected_object_name, str(post))
+    def test_blog_name_is_title(self):        
+        blog= Blog.objects.get(id=1)
+        expected_object_name = blog.title
+        self.assertEquals(expected_object_name, str(blog))
 
     # def test_get_absolute_url(self):
-    #     post= Post.objects.get(id=1)
-    #     self.assertEquals(post.get_absolute_url(), '/blog/1')
+    #     blog= Blog.objects.get(id=1)
+    #     self.assertEquals(Blog.get_absolute_url(), '/blog/1')
 
 class CommentModelTest(TestCase):
     @classmethod
@@ -113,7 +113,7 @@ class CommentModelTest(TestCase):
         self.assertEquals(field_label, 'author')
 
     def test_date_posted_label(self):
-        field_label = Post._meta.get_field('date_posted').verbose_name
+        field_label = Blog._meta.get_field('date_posted').verbose_name
         self.assertEquals(field_label, 'date posted')
 
     def test_last_edit_label(self):
@@ -129,7 +129,7 @@ class CommentModelTest(TestCase):
         self.assertEquals(max_length, 500)
 
     # custom class methods
-    def test_comment_name_is_description(self):
+    def test_comment_name_is_first_seventy_five_chars_of_description(self):
         comment = Comment.objects.get(id=1)
-        expected_object_name = comment.description
+        expected_object_name = comment.description[:75]
         self.assertEquals(expected_object_name, str(comment))
