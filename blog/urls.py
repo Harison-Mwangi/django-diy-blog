add2905ea2f22eaf1c81fcc2abf7eb3069e86be9
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-07-16 22:03:22 +0300

    Added URL configuration for the Blogger and Blog detail views

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 
+    path('blogger/<int:pk>/', views.BloggerDetailView.as_view(), name='blogger-detail'),
     path('bloggers/', views.BloggerListView.as_view(), name='bloggers'),
+
+    path('blog/<int:pk>/', views.BlogDetailView.as_view(), name='blog-detail'),
     path('blogs/', views.BlogListView.as_view(), name='blogs'),
 ]
